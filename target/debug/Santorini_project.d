068a6ac4053bc34e1cf012af0a54a9cc4072ae23
pameirelles <rafaelpmeirelles@gmail.com>
2022-08-16 19:10:55 -0300

    first commit

---
+++ target/debug/Santorini_project.d
@@ -0,0 +1,1 @@
+C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\Santorini_project.exe: C:\Users\rafae\IdeaProjects\Santorini_project\src\main.rs
---
+++ target/debug/deps/Santorini_project.d
@@ -0,0 +1,5 @@
+C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.exe: src\main.rs
+
+C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.d: src\main.rs
+
+src\main.rs:
