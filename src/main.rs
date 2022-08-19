981fc6a1e2a49ba8437a584da50ad657b4523de7
pameirelles <rafaelpmeirelles@gmail.com>
2022-08-19 13:38:57 -0300

    setting up gui

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use crate::board::{make_move, new_board, print_board, print_move};
 use crate::rules::{gen_all_moves, gen_move, init_neighbours, Neighbours};
-
+use eframe::run_native;
 mod board;
 mod rules;
+mod visual;
+use eframe::egui;
+
+
+#[derive(Default)]
+struct MyEguiApp {}
 
 fn main() {
-    let n:Neighbours = init_neighbours();
-    let mut b1: board::Board = new_board([14, 15, 9, 19]);
-    let v1: Vec<board::Move> = gen_all_moves(&mut b1, &1, &n);
-    for mv in v1{
-        print_move(&mv);
-    }
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
 }
-
--- target/debug/deps/Santorini_project.d
+++ target/debug/deps/Santorini_project.d
@@ -1,7 +1,8 @@
-C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.exe: src\main.rs src\board.rs src\rules.rs
+C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.exe: src\main.rs src\board.rs src\rules.rs src\visual.rs
 
-C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.d: src\main.rs src\board.rs src\rules.rs
+C:\Users\rafae\IdeaProjects\Santorini_project\target\debug\deps\Santorini_project.d: src\main.rs src\board.rs src\rules.rs src\visual.rs
 
 src\main.rs:
 src\board.rs:
 src\rules.rs:
+src\visual.rs:
