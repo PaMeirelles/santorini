bfd3ce726b956d8ded7f33b8eb8a132ab3125664
pameirelles <rafaelpmeirelles@gmail.com>
2022-08-18 23:52:47 -0300

    make_move

--- src/board.rs
+++ src/board.rs
@@ -8,11 +8,11 @@ pub struct Board {
 pub fn new_board(workers:[i32; 4]) -> Board{
     Board {
         blocks: [0; 25],
-        workers
+        workers,
     }
 }
 
-pub fn print_blocks(blocks:[i32;25]){
+pub fn print_blocks(blocks:&[i32;25]){
    let mut counter:i32 = 0;
     for block in blocks{
         print!("{} ", block);
@@ -23,15 +23,15 @@ pub fn print_blocks(blocks:[i32;25]){
     }
 }
 
-pub fn print_workers(workers:[i32;4]){
+pub fn print_workers(workers:&[i32;4]){
     println!("[{}, {}], [{}, {}]", workers[0],workers[1], workers[2], workers[3]);
 }
 
-pub fn print_board(board:Board){
+pub fn print_board(board:&Board){
     println!("Blocks: ");
-    print_blocks(board.blocks);
+    print_blocks(&board.blocks);
     println!("Workers: ");
-    print_workers(board.workers);
+    print_workers(&board.workers);
 }
 pub struct Move {
     pub from: i32,
@@ -46,17 +46,27 @@ pub fn new_move(from:i32, to:i32, build:i32) -> Move{
         build,
     }
 }
-pub fn get_square(id:i32) -> String {
+pub fn get_square(id:&i32) -> String {
     let letters: [char; 5] = ['A', 'B', 'C', 'D', 'E'];
     let numbers: [char; 5] = ['1', '2', '3', '4', '5'];
-    let x:i32 = id % 5;
-    let y:i32 = id / 5;
+    let x:i32 = *id % 5;
+    let y:i32 = *id / 5;
     let mut str = String::new();
     str.push(letters[y as usize]);
     str.push(numbers[x as usize]);
     return str;
 }
-pub fn print_move(mv:Move){
-    println!("{}({}) -> {}({}) [{}({})]", mv.from, get_square(mv.from), mv.to,
-                     get_square(mv.to), mv.build, get_square(mv.build));
+pub fn print_move(mv:&Move){
+    println!("{}({}) -> {}({}) [{}({})]", &mv.from, get_square(&mv.from), &mv.to,
+                     get_square(&mv.to), &mv.build, get_square(&mv.build));
+}
+
+pub fn make_move(mv:&Move, board: &mut Board){
+    for worker in board.workers.iter_mut(){
+        if *worker == mv.from {
+            *worker = mv.to;
+            break;
+        }
+    }
+    board.blocks[mv.build as usize] += 1;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use crate::board::{new_board, print_blocks, print_board, print_workers};
+use crate::board::{make_move, new_board, print_blocks, print_board, print_workers};
 
 mod board;
 
 fn main() {
-    let b1: board::Board = new_board([14, 15, 9, 19]);
-    let m1: board::Move = board::new_move(10, 16, 21);
-    board::print_move(m1);
-    print_board(b1);
+    let mut b1: board::Board = new_board([14, 15, 9, 19]);
+    let m1: board::Move = board::new_move(15, 16, 21);
+    board::print_move(&m1);
+    print_board(&b1);
+    make_move(&m1, &mut b1);
+    print_board(&b1);
 }
 
 
