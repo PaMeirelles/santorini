0ce0a121615a01bdc5dfdabfc4ed00dd21858f95
pameirelles <rafaelpmeirelles@gmail.com>
2022-08-20 11:34:22 -0300

    game manager

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -1,7 +1,24 @@
-use crate::board::{Board, Move};
-use crate::rules::{Neighbours};
+use crate::board::{Board, Move, new_board, new_move};
+use crate::rules::{Neighbours, init_neighbours};
 use crate::engine::{get_best_move};
+use crate::{make_move, print_board, print_move};
 
-pub fn play_game(eval1:fn(board:&Board, nei:&Neighbours) -> f64, eval2:fn(board:&Board, nei:&Neighbours) -> f64){
+pub fn play_game(eval1:fn(board:&Board, nei:&Neighbours) -> f64, eval2:fn(board:&Board, nei:&Neighbours) -> f64, b1: &mut Board, n: &Neighbours){
+    let mut best:Move = new_move(&-1, &0, &-1);
+    let mut color:i32 = 1;
 
+    loop {
+        if color == 1 {
+            best = get_best_move(b1, 4, 1, n, eval1)
+        } else {
+            best = get_best_move(b1, 4, -1, n, eval2)
+        }
+        print_move(&best);
+        make_move(&best, b1);
+        print_board(&b1);
+        if best.build == -2 {
+            break;
+        }
+        color *= -1;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,15 @@ mod board;
 mod rules;
 mod visual;
 mod engine;
+
 mod game_manager;
 
 use piston_window::*;
 use crate::engine::{negamax, random_eval, neighbour_high, get_best_move};
+use crate::game_manager::play_game;
 
 fn main() {
-    /*
-    let mut window: PistonWindow =
-        WindowSettings::new("Santorini", [600, 600])
-            .exit_on_esc(true).build().unwrap();
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g, _device| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 1.0, 0.0, 1.0], // red
-                      [0.0, 100.0, 100.0, 100.0],
-                      c.transform, g);
-        });
-    }
-    */
     let mut b1:Board = new_board([0, 4, 7, 11]);
     let n:Neighbours = init_neighbours();
-    let mut best:Move = new_move(&-1, &0, &-1);
-
-    b1.blocks[7] = 2;
-    b1.blocks[8] = 3;
-    println!("{}", neighbour_high(&b1, &n));
-
-    best = get_best_move(&mut b1, 1, -1, &n, neighbour_high);
-    print_move(&best);
-    make_move(&best, &mut b1);
-    println!("{}", neighbour_high(&b1, &n));
+    play_game(neighbour_high, neighbour_high, &mut b1, &n);
 }
